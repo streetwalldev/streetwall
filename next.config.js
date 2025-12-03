42d4cdb675b8878239a6ad91931e1d1092ec4525
streetwalldev <guitarlinerec@gmail.com>
2025-12-03 22:24:48 +0500

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // если захотите static export позже
+  reactStrictMode: false,
+};
+
+export default nextConfig;
