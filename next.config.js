09d36ebacc155735347308129e6b48879b7dda62
streetwalldev <guitarlinerec@gmail.com>
2025-12-14 12:31:34 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    return config;
+  },
   output: 'export', // если захотите static export позже
   reactStrictMode: false,
 };
 
 export default nextConfig;
+module.exports = nextConfig; 
